6cd8ef1d67d729e1160eb5ec1c8272fec284a96a
İlayda Yıldız <146078544+ilaydayilidz778@users.noreply.github.com>
2024-02-07 23:00:06 +0300

    Entity classes have been created, and relationships have been established. Seed data has been added, and the database has been created and populated using the migration-update database command.

---
+++ EasyBlog/Data/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyBlog.Data
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        // Navigation Property : Post (one-to-many relationship)
+        public List<Post> Posts { get; set; } = new();
+    }
+}
---
+++ EasyBlog/Data/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace EasyBlog.Data
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string Content { get; set; } = null!;
+        public string? Image { get; set; } 
+
+        // Navigation Property : Category (one-to-many relationship)
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+    }
+}
